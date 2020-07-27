233f4cdd423182b7b3202ad108d69168daaba976
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-27 15:19:49 +0900

    feat(HomePage): render categories
    - 판매중인 케이크 토퍼 카테고리들을 렌더링하도록 구현

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 
 export default function HomePage() {
   return (
-    <h1>YenTopper</h1>
+    <>
+      <h1>YenTopper</h1>
+      <ul>
+        <li>생일</li>
+        <li>육아</li>
+        <li>여행</li>
+        <li>결혼</li>
+      </ul>
+    </>
   );
 }
