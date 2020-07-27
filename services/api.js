f0f1c3c53a9adbef9208b3e89c708edbb2aac218
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-27 18:26:42 +0900

    debug(api): add parse json
    - fetch로 받아온 데이터를 처리하는 기능 추가

--- services/api.js
+++ services/api.js
@@ -1,8 +1,9 @@
-const API_SERVER = 'http://3.34.86.117:8000/';
+const API_SERVER = 'http://3.34.86.117:8000';
 
 export async function fetchCategories() {
   const url = `${API_SERVER}/categories`;
-  const categories = await fetch(url);
+  const response = await fetch(url);
+  const data = await response.json();
 
-  return categories;
+  return data;
 }
