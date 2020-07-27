93490ea5e1e503f3f0c1a2c652cda651d8323d42
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-27 18:50:54 +0900

    test(HomePage): mouse over on category list
    - 카테고리 목록에 마우스 오버 이벤트가 발생했을 때 특정 함수를 알맞는 인자로 실행하는지 테스트

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
 import categoriesFixture from '../fixtures/categories';
 
 describe('HomePage', () => {
+  const handleMouseOver = jest.fn();
+
   function renderHomePage() {
     return render((
-      <HomePage categories={categoriesFixture} />
+      <HomePage
+        categories={categoriesFixture}
+        onMouseOver={handleMouseOver}
+      />
     ));
   }
 
@@ -25,5 +30,17 @@ describe('HomePage', () => {
     categoriesFixture.forEach((category) => {
       expect(container).toHaveTextContent(category.name);
     });
+  });
+
+  it('listens the mouse over event on categories', () => {
+    const categoryFixtureIndex = 0;
+    const categoryName = categoriesFixture[categoryFixtureIndex].name;
+    const categoryId = categoriesFixture[categoryFixtureIndex].id;
+
+    const { getByText } = renderHomePage();
+
+    fireEvent.mouseOver(getByText(categoryName));
+
+    expect(handleMouseOver).toBeCalledWith(categoryId);
   });
 });
