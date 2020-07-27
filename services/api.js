0379db7b27e39c1573dfee93957ef47f540d2bda
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-27 18:21:10 +0900

    feat(api): fetchCategories
    - 외부 서버에서 카테고리 목록을 받아오도록 구현

--- services/api.js
+++ services/api.js
@@ -1,5 +1,10 @@
+const API_SERVER = 'http://3.34.86.117:8000/';
+
 export async function fetchCategories() {
-  return null;
+  const url = `${API_SERVER}/categories`;
+  const categories = await fetch(url);
+
+  return categories;
 }
 
 export async function xxx() {
--- services/api.test.js
+++ services/api.test.js
@@ -4,6 +4,8 @@ import categoriesFixture from '../fixtures/categories';
 
 describe('fetchCategories', () => {
   it('get categories info from api server', async () => {
+    global.fetch = jest.fn().mockReturnValue(categoriesFixture);
+
     const categories = await fetchCategories();
 
     expect(categories).toEqual(categoriesFixture);
