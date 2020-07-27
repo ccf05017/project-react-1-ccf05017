e46f775185ff65a7498be805f720579beeaf646a
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-27 17:08:25 +0900

    refactor(slice): getInitialState
    - 최초 상태로 categories를 불러오기 위해 redux-thunk를 이용한 테스트 케이스 작성

--- src/slice.test.jsx
+++ src/slice.test.jsx
@@ -1,9 +1,16 @@
+import thunk from 'redux-thunk';
+
+import configureStore from 'redux-mock-store';
+
 import reducer, {
   getInitialState,
   setCategories,
 } from './slice';
 
 import categoriesFixture from '../fixtures/categories';
+
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
 
 describe('reducer', () => {
   context('when the application first loaded', () => {
@@ -18,18 +25,6 @@ describe('reducer', () => {
     });
   });
 
-  describe('getInitialState', () => {
-    it('can get initial states', () => {
-      const initialState = {
-        categories: [],
-      };
-
-      const state = reducer(initialState, getInitialState());
-
-      expect(state.categories).toHaveLength(4);
-    });
-  });
-
   describe('setCategories', () => {
     it('can set categories', () => {
       const initialState = {
@@ -39,6 +34,24 @@ describe('reducer', () => {
       const state = reducer(initialState, setCategories(categoriesFixture));
 
       expect(state.categories).toEqual(categoriesFixture);
+    });
+  });
+});
+
+describe('async actions', () => {
+  let store;
+
+  describe('getInitialState', () => {
+    beforeEach(() => {
+      store = mockStore({});
+    });
+
+    it('can get initial states', async () => {
+      await store.dispatch(getInitialState());
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setCategories(categoriesFixture));
     });
   });
 });
