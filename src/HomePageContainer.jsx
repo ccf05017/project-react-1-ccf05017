94064e68877f0b78e7d23cdaba83be1ab0cccac0
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-27 15:39:23 +0900

    feat(HomePageContainer): render HomePage
    - 렌더링을 담당하는 컴포넌트를 불러오도록 구현

--- src/HomePageContainer.jsx
+++ src/HomePageContainer.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
+import HomePage from './HomePage';
+
 export default function HomePageContainer() {
+  const categories = [];
+
   return (
-    <div />
+    <HomePage categories={categories} />
   );
 }
