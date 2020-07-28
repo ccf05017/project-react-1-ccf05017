adbee715a2daf8d9490826dab785e920c8b03ee2
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-28 09:51:14 +0900

    feat(CategoryItemContainer): render selected
    - 카테고리가 선택됐을 때 선택 표기가 된 상태로 렌더링하도록 기능 구현

--- src/CategoryItemContainer.jsx
+++ src/CategoryItemContainer.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import CategoryItem from './CategoryItem';
 
 export default function CategoryItemContainer({ category, onMouseOver }) {
+  const { selectedCategory } = useSelector((state) => ({
+    selectedCategory: state.selectedCategory,
+  }));
+
+  const isSelected = selectedCategory.id === category.id;
+
   return (
-    <CategoryItem
-      category={category}
-      onMouseOver={onMouseOver}
-    />
+    <>
+      <CategoryItem
+        category={category}
+        onMouseOver={onMouseOver}
+      />
+      {isSelected && '(O)'}
+    </>
   );
 }
