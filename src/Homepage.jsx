fe04a4f6885b8f4d71ae81bfde103e2da3b4e7a9
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-29 08:58:51 +0900

    feat(Homepage): render homepage
    - 홈페이지를 구성하는 카테고리 목록, 상품 목록을 렌더링

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import CategoryListContainer from './CategoryListContainer';
+import ProductListContainer from './ProductListContainer';
+
 export default function Homepage() {
   return (
-    <p>heloo</p>
+    <>
+      <CategoryListContainer />
+      <ProductListContainer />
+    </>
   );
 }
--- src/Homepage.test.jsx
+++ src/Homepage.test.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 import { render } from '@testing-library/react';
 
 import Homepage from './Homepage';
@@ -7,12 +9,22 @@ import Homepage from './Homepage';
 import categoriesFixture from '../fixtures/categories';
 import productsFixture from '../fixtures/products';
 
+jest.mock('react-redux');
+
 describe('Homepage', () => {
   const selectedCategoryIndex = 0;
   const selectedCategory = categoriesFixture[selectedCategoryIndex];
   const selectedProductList = productsFixture.filter(
     (product) => selectedCategory.id === product.typeId,
   );
+
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      selectedCategory,
+      products: productsFixture,
+      categories: categoriesFixture,
+    }));
+  });
 
   it('renders the homepage', () => {
     const categoryListHeader = 'YenTopper';
