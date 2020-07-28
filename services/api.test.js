de68ecba982caa1a6383f14afb47ba0e11f77b80
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-28 16:09:53 +0900

    test(api): fetchProducts
    - 외부 API 서버에서 상품 정보를 가져오는지 테스트

--- services/api.test.js
+++ services/api.test.js
@@ -1,6 +1,7 @@
-import { fetchCategories } from './api';
+import { fetchCategories, fetchProducts } from './api';
 
 import categoriesFixture from '../fixtures/categories';
+import productsFixture from '../fixtures/products';
 
 describe('fetchCategories', () => {
   it('get categories info from api server', async () => {
@@ -11,5 +12,17 @@ describe('fetchCategories', () => {
     const categories = await fetchCategories();
 
     expect(categories).toEqual(categoriesFixture);
+  });
+});
+
+describe('fetchProducts', () => {
+  it('get products info from api server', async () => {
+    global.fetch = jest.fn().mockReturnValue({
+      async json() { return productsFixture; },
+    });
+
+    const products = await fetchProducts();
+
+    expect(products).toEqual(productsFixture);
   });
 });
