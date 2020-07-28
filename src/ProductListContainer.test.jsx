da64960a83d7073dfab88c64b6f19a617706376b
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-28 16:52:17 +0900

    test(ProductListContainer): filter products
    - 선택된 카테고리 별로 필터링이 정상적으로 되는지 확인하는 테스트 추가

--- src/ProductListContainer.test.jsx
+++ src/ProductListContainer.test.jsx
@@ -7,24 +7,60 @@ import { useSelector } from 'react-redux';
 import ProductListContainer from './ProductListContainer';
 
 import productsFixture from '../fixtures/products';
+import categoriesFixture from '../fixtures/categories';
 
 jest.mock('react-redux');
 
 describe('ProductListContainer', () => {
-  beforeEach(() => {
-    useSelector.mockImplementation((selector) => selector({
-      products: productsFixture,
-    }));
+  context('without selected category', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        products: productsFixture,
+        selectedCategory: null,
+      }));
+    });
+
+    it('renders the all products', () => {
+      const { container } = render((
+        <ProductListContainer />
+      ));
+
+      productsFixture.forEach((productFixture) => {
+        expect(container).toHaveTextContent(productFixture.title);
+        expect(container).toHaveTextContent(productFixture.price);
+      });
+    });
   });
 
-  it('renders the products', () => {
-    const { container } = render((
-      <ProductListContainer />
-    ));
+  context('with selected category', () => {
+    const selectedCategoryIndex = 0;
+    const notSelectedCategoryIndex = 1;
+    const selectedCategory = categoriesFixture[selectedCategoryIndex];
+    const notSelectedCategory = categoriesFixture[notSelectedCategoryIndex];
 
-    productsFixture.forEach((productFixture) => {
-      expect(container).toHaveTextContent(productFixture.title);
-      expect(container).toHaveTextContent(productFixture.price);
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        products: productsFixture,
+        selectedCategory,
+      }));
+    });
+
+    it('renders the products which is filtered by category', () => {
+      function getFilteredProduct(category) {
+        return productsFixture.find(
+          (productFixture) => productFixture.typeId === category.id,
+        );
+      }
+
+      const { container } = render((
+        <ProductListContainer />
+      ));
+
+      expect(container).toHaveTextContent(getFilteredProduct(selectedCategory).title);
+      expect(container).toHaveTextContent(getFilteredProduct(selectedCategory).price);
+
+      expect(container).not.toHaveTextContent(getFilteredProduct(notSelectedCategory).title);
+      expect(container).not.toHaveTextContent(getFilteredProduct(notSelectedCategory).price);
     });
   });
 });
