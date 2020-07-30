80263027c8d7a5cfc7a231b2b6fe834ff219062e
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-30 09:28:54 +0900

    feat(RoutePage): link to main
    - 클릭 시 루트 경로로 이동하는 링크 추가

--- src/RoutePage.jsx
+++ src/RoutePage.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import {
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 import Homepage from './Homepage';
 
 export default function RoutePage() {
   return (
     <>
-      <h1>YenTopper</h1>
+      <h1>
+        <Link to="/">
+          YenTopper
+        </Link>
+      </h1>
       <Switch>
         <Route to="/" component={Homepage} />
       </Switch>
