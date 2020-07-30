14fb08f26c836bb49ad4707201a584016d062758
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-30 10:34:13 +0900

    feat(ProductDetail): render product detail
    - 상품 상세 정보를 렌더링하는 기능 구현

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { productFixture } from '../fixtures/products';
+
 export default function ProductDetail() {
   return (
-    <h2>상품 상세</h2>
+    <>
+      <h2>상품 상세</h2>
+      <p>
+        상품명:
+        {' '}
+        {productFixture.title}
+      </p>
+      <p>
+        가격:
+        {' '}
+        {productFixture.price}
+        {' '}
+        원
+      </p>
+    </>
   );
 }
