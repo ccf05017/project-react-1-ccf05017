b9d439148e9732383dc741e6f2b1c13b73d559b9
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-30 15:41:03 +0900

    refactor(ProductList): handleClick
    - 요소를 클릭했을 때 페이지 이동하는 기능 구현

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import ProductItem from './ProductItem';
 
 export default function ProductList({ products }) {
+  const history = useHistory();
+
+  function handleClick(product) {
+    const url = `/products/${product.id}`;
+
+    return (event) => {
+      event.preventDefault();
+      history.push(url);
+    };
+  }
+
   return (
     <>
       <h2>상품</h2>
       <ul>
         {products.map((product) => (
-          <Link
+          <ProductItem
             key={product.id}
-            to={`/products/${product.id}`}
-          >
-            <ProductItem
-              product={product}
-            />
-          </Link>
+            product={product}
+            handleClick={handleClick(product)}
+          />
         ))}
       </ul>
     </>
