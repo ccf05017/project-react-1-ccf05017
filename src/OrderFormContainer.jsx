17eaedabcb58da05e35ee2a0c9f26498892e2977
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-31 13:16:04 +0900

    test(OrderFormContainer): render form value
    - 폼에 데이터가 입력됐을 때 정상적으로 리덕스 상태로부터 상태를 가져와서 렌더링하는지 확인하는 테스트

--- src/OrderFormContainer.jsx
+++ src/OrderFormContainer.jsx
@@ -8,7 +8,7 @@ export default function OrderFormContainer() {
       <label htmlFor="phoneNumber">주문자 연락처</label>
       <input type="text" name="phoneNumber" />
       <label htmlFor="amount">주문수량</label>
-      <input type="text" name="amount" />
+      <input type="number" name="amount" />
       <label htmlFor="address">배송지</label>
       <input type="text" name="address" />
       <button type="button">
--- src/OrderFormContainer.test.jsx
+++ src/OrderFormContainer.test.jsx
@@ -2,22 +2,61 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import OrderFormContainer from './OrderFormContainer';
 
+jest.mock('react-redux');
+
 describe('OrderFormContainer', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      orderForm: {
+        username: given.username,
+        phoneNumber: null,
+        amount: 0,
+        address: null,
+      },
+    }));
+  });
+
   function renderOrderFormContainer() {
     return render((
       <OrderFormContainer />
     ));
   }
 
-  it('renders the order form', () => {
-    const { container } = renderOrderFormContainer();
+  const username = '홍길동';
 
-    expect(container).toHaveTextContent('주문자 이름');
-    expect(container).toHaveTextContent('주문자 연락처');
-    expect(container).toHaveTextContent('주문수량');
-    expect(container).toHaveTextContent('배송지');
-    expect(container).toHaveTextContent('주문하기');
+  context('without written info', () => {
+    it('renders the order form template', () => {
+      given('username', () => null);
+
+      const { container } = renderOrderFormContainer();
+
+      expect(container).toHaveTextContent('주문자 이름');
+      expect(container).toHaveTextContent('주문자 연락처');
+      expect(container).toHaveTextContent('주문수량');
+      expect(container).toHaveTextContent('배송지');
+      expect(container).toHaveTextContent('주문하기');
+
+      expect(container).not.toHaveTextContent(username);
+    });
+  });
+
+  context('with written info', () => {
+    it('renders the order form template and value', () => {
+      given('username', () => username);
+
+      const { container } = renderOrderFormContainer();
+
+      expect(container).toHaveTextContent('주문자 이름');
+      expect(container).toHaveTextContent('주문자 연락처');
+      expect(container).toHaveTextContent('주문수량');
+      expect(container).toHaveTextContent('배송지');
+      expect(container).toHaveTextContent('주문하기');
+
+      expect(container).toHaveTextContent(username);
+    });
   });
 });
