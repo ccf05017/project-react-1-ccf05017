d77d58a5456ff3304db6a4d30c37d8f73f404fd2
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-31 13:07:45 +0900

    feat(OrderFormContainer): render form
    - 사용자가 상품을 주문하기 위한 주문 폼의 기본 형태 렌더링

--- src/OrderFormContainer.jsx
+++ src/OrderFormContainer.jsx
@@ -2,6 +2,18 @@ import React from 'react';
 
 export default function OrderFormContainer() {
   return (
-    <p>hello</p>
+    <>
+      <label htmlFor="username">주문자 이름</label>
+      <input type="text" name="username" />
+      <label htmlFor="phoneNumber">주문자 연락처</label>
+      <input type="text" name="phoneNumber" />
+      <label htmlFor="amount">주문수량</label>
+      <input type="text" name="amount" />
+      <label htmlFor="address">배송지</label>
+      <input type="text" name="address" />
+      <button type="button">
+        주문하기
+      </button>
+    </>
   );
 }
