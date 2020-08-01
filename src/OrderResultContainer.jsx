a046d619f1c8e8a4faa3220d8e14c1c665f11717
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-01 11:11:31 +0900

    feat(OrderResultContainer): render order success
    - 사용자에게 주문 결과를 알려주기 위해 주문이 성공했을 때 주문 성공 메시지를 렌더링한다

--- src/OrderResultContainer.jsx
+++ src/OrderResultContainer.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function OrderResultContainer() {
+  const orderSuccessMessage = '주문이 완료됐습니다.';
+  const bankAccount = '국민은행: 00-0000-0000-00';
+
+  const { orderResult } = useSelector((state) => ({
+    orderResult: state.orderResult,
+  }));
+
+  function makeResultMessage() {
+    return (
+      <>
+        <p>{orderSuccessMessage}</p>
+        <p>{`${bankAccount}로 입금해주세요`}</p>
+      </>
+    );
+  }
+
   return (
-    <p>hello</p>
+    <div>{makeResultMessage()}</div>
   );
 }
