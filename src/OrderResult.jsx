1d163e877076103419727e3e61d21acd1a4fa7e5
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-01 11:23:31 +0900

    refactor(OrderResult): extract success, fail
    - 주문이 성공했을 때 화면, 실패했을 때 화면을 별도의 컴포넌트로 분리

--- src/OrderResult.jsx
+++ src/OrderResult.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
 
+import OrderSuccessMessage from './OrderSuccessMessage';
+
+import OrderFailMessage from './OrderFailMessage';
+
 export default function OrderResult({ orderResult }) {
-  const orderSuccessMessage = '주문이 완료됐습니다.';
-  const bankAccount = '국민은행: 00-0000-0000-00';
-
-  const orderFailMessage = '주문이 실패했습니다.';
-
-  function makeResultMessage() {
-    if (orderResult) {
-      return (
-        <>
-          <p>{orderSuccessMessage}</p>
-          <p>{`${bankAccount}로 입금해주세요`}</p>
-        </>
-      );
-    }
-
-    return (
-      <>
-        <p>{orderFailMessage}</p>
-      </>
-    );
-  }
-
   return (
-    <div>{makeResultMessage()}</div>
+    <div>
+      {
+        (orderResult)
+          ? <OrderSuccessMessage />
+          : <OrderFailMessage />
+      }
+    </div>
   );
 }
