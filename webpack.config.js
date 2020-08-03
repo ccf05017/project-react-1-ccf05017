47ed8baf3220c00a32f6441929579aec272fd994
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-03 14:09:22 +0900

    chore(cicd): modify webpack and git actions
    - 배포를 위해 웹팩, 깃헙 액션 수정

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
   output: {
-    filename: '[name].js',
-    path: path.resolve('./build'),
+    filename: 'bundle-[hash].js',
+    path: path.resolve('./dist'),
   },
   module: {
     rules: [
@@ -18,6 +19,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
   devServer: {
     historyApiFallback: {
       index: 'index.html',
