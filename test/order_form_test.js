07326d46d817cb61d6d1932c607e2a3c05429065
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-07 09:02:43 +0900

    test(e2e): clear order result
    - 상품 주문후 다른 상품 상세 페이지로 이동했을 때, 이전 주문 결과가 보이지 않는지 확인하는 테스트

--- test/order_form_test.js
+++ test/order_form_test.js
@@ -23,3 +23,21 @@ Scenario('주문서를 작성해서 주문할 수 있다.', (I) => {
     I.dontSeeInField(fieldName, value);
   });
 });
+
+Scenario('주문 완료 후 다른 상품 상세 조회 했을 때, 이전 주문 결과가 보이지 않는다.', (I) => {
+  I.amOnPage('/products/1');
+
+  Object.entries(formData).forEach(([fieldName, value]) => {
+    I.fillField(fieldName, value);
+  });
+
+  I.click('주문하기');
+  I.see('주문이 완료됐습니다.');
+  I.see('국민은행: 00-0000-0000-00');
+
+  I.click('YenTopper');
+  I.click('생일축하해');
+
+  I.dontSee('주문이 완료됐습니다.');
+  I.dontSee('국민은행: 00-0000-0000-00');
+});
