ad9e4fb11b75598ede43eedfa675c2e278a3509e
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-10 09:20:21 +0900

    feat(Header): styling header
    - 메인 페이지로 돌아가는 헤더 링크 스타일링
    - 가운데 정렬 + 보기 싫은 밑줄 제거

---
+++ src/Header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import {
+  Link,
+} from 'react-router-dom';
+
+import styled from '@emotion/styled';
+
+const MainNav = styled.div({
+  display: 'flex',
+  height: '44px',
+  padding: '0 1rem',
+});
+
+const MainLink = styled('h1')`
+  align-items: center;
+  max-width: 1000px;
+  height: 100%;
+  margin: 0 auto;
+  justify-content: space-between;
+  & > a {
+    text-decoration: none;
+  }
+`;
+
+export default function Header() {
+  return (
+    <MainNav>
+      <MainLink>
+        <Link to="/">
+          YenTopper
+        </Link>
+      </MainLink>
+    </MainNav>
+  );
+}
--- src/RoutePage.jsx
+++ src/RoutePage.jsx
@@ -3,20 +3,16 @@ import React from 'react';
 import {
   Switch,
   Route,
-  Link,
 } from 'react-router-dom';
 
+import Header from './Header';
 import HomepageContainer from './HomepageContainer';
 import ProductDetailContainer from './ProductDetailContainer';
 
 export default function RoutePage() {
   return (
     <>
-      <h1>
-        <Link to="/">
-          YenTopper
-        </Link>
-      </h1>
+      <Header />
       <Switch>
         <Route exact path="/" component={HomepageContainer} />
         <Route path="/products/:id" component={ProductDetailContainer} />
