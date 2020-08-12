b496400e65661784922bd2c822c8842c63378737
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-08-12 09:29:58 +0900

    refactor(App): move font to css file
    - 전체 App에 적용하기 위한 CSS 설정들을 별도의 기본 CSS 파일로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
 import React from 'react';
 
-import styled from '@emotion/styled';
 import RoutePage from './RoutePage';
-
-const GlobalStyleContainer = styled.div({
-  fontFamily: "'Apple SD Gothic Neo', Roboto, 'Noto Sans KR', NanumGothic, 'Malgun Gothic', sans-serif",
-  lineHeight: '1.2',
-  wordWrap: 'break-word',
-});
 
 export default function App() {
   return (
-    <GlobalStyleContainer>
-      <RoutePage />
-    </GlobalStyleContainer>
+    <RoutePage />
   );
 }
